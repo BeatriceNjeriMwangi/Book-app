e365fa08fa435278636de984f98ff29df428569c
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2023-11-22 12:46:17 +0300

    commit

--- src/pages/CartItem.js
+++ src/pages/CartItem.js
@@ -1,23 +1,14 @@
-export default function CartItem() {
+import React from 'react';
 
-    const [cart, setCart] = useState([]);
-    const [total, setTotal] = useState(0);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        setCart(JSON.parse(localStorage.getItem('cart')));
-    }, []);
-    useEffect(() => {
-        setTotal(cart.reduce((total, item) => total + item.price * item.quantity, 0));
-    }, [cart]);
-    return (
-        <div className="cart-item">
-            <div className="cart-item-header">
-                <h3>Cart</h3>
-                <div className="cart-item-header-right">
-                    <button className="btn btn-primary" onClick={() => setLoading(true)}>Checkout</button>
-                </div>
-            </div>
-        </div>  
-        )      
+function CartItem({ item }) {
+  return (
+    <li className="cart-item">
+      <span>{item.title}</span>
+      <span>Quantity: {item.quantity}</span>
+      <span>${item.price * item.quantity}</span>
+      {/* Add a button to remove the item from the cart */}
+    </li>
+  );
 }
+
+export default CartItem;
