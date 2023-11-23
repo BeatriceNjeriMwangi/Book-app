2d99a2ac9e7fc132146875537d16c4c389ff8fb9
evansthitai <evansnt9619@gmail.com>
2023-11-23 21:51:27 +0300

    searchbar working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,35 @@
-import React, { useState  } from 'react';
+import React, { useState } from 'react';
 
+function Header({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // Pass the search term to the parent component (App.js)
+    onSearch(searchTerm);
+  };
 
-function Header(){
-    const [books, setBooks] = useState([]);
-    const filterBooks = () => books.filter((book) => {
-        setBooks(book)
-    })
-    return(
-        <header>
-            <div className='form'>
-            <h1>Books Haven</h1>
-            <form>
-                <div className='search-barg'>
-                <label for="search" ></label>
-                 <input type="text" id="search" name="search" placeholder="Enter your search" />
-                 <button id='search-button' onClick={filterBooks}>Search</button>
-                 </div>
-            </form>
-            </div>
-
-        </header>
-    )
+  return (
+    <header>
+      <div className='form'>
+        <h1>Books Haven</h1>
+        <form onSubmit={handleSearch}>
+          <div className='search-bar'>
+            <label htmlFor="search"></label>
+            <input
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Enter your search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button type='submit' id='search-button'>Search</button>
+          </div>
+        </form>
+      </div>
+    </header>
+  );
 }
+
 export default Header;
