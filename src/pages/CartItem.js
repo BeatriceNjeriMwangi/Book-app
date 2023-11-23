cb7315429db89702f69845e96f67e313ce0decf9
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2023-11-23 11:11:44 +0300

    added remove item functionality

--- src/pages/CartItem.js
+++ src/pages/CartItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const CartItem = ({ item, onRemoveItem }) => {
+
+     onRemoveItem = (item) => {
+        return(item.id !== item.key);
+    }; 
+    
+
+
     return (
         <div>
-            <li className="cart-item">
+            <li className="cart-item" key={item.id}>
                 <span>{item.title}</span>
                 <img src={item.image_url} alt='book' />
                 <span>{item.price}</span>
-                <button onClick={() => onRemoveItem(item.id)}>Remove from cart</button>
+                <button onClick={() => onRemoveItem()}>Remove from cart</button>
             </li>
         </div>
     );
