d10a57ba9760ab28aeae163e60f6d1a24044db9b
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2023-11-23 22:42:38 +0300

    cart purchase

--- src/pages/CartItem.js
+++ src/pages/CartItem.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CartItem = ({ item, onRemoveItem }) => {
+const CartItem = ({ item, onRemoveItem, onBuyItem }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [isPurchaseSuccessful, setPurchaseSuccessful] = useState(false);
 
-     onRemoveItem = (item) => {
-        return(item.id !== item.key);
-    }; 
+  const handleRemoveItem = () => {
+    onRemoveItem(item.id);
+  };
 
+  const handleBuyClick = () => {
+    onBuyItem(item, quantity);
 
+    // Simulate a successful purchase
+    setPurchaseSuccessful(true);
 
-    return (
-        <div>
-            <li className="cart-item" key={item.id}>
-                <span>{item.title}</span>
-                <img src={item.image_url} alt='book' />
-                <span>{item.price}</span>
-                <button onClick={() => onRemoveItem()}>Remove from cart</button>
-            </li>
-        </div>
-    );
+    // Reset purchase status after a delay
+    setTimeout(() => {
+      setPurchaseSuccessful(false);
+    }, 2000);
+  };
+
+  return (
+    <div>
+      <li className="cart-item" key={item.id}>
+        <span>{item.title}</span>
+        <img src={item.image_url} alt="book" />
+        <span>{item.price}</span>
+        <input
+          type="number"
+          min="1"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <button onClick={handleRemoveItem}>Remove from cart</button>
+        <button onClick={handleBuyClick}>Buy</button>
+
+        {isPurchaseSuccessful && (
+          <div className="purchase-success-popup">
+            Purchase successful!
+          </div>
+        )}
+      </li>
+    </div>
+  );
 };
-export default CartItem
+
+export default CartItem;
